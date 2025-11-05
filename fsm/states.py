ef9431479f135d3a702bb6facf6638e16d246f9e
Jayson Ndungu <jaysongachiani@gmail.com>
2025-11-04 19:45:56 -0500

    Completed state machine and defined transitions

--- fsm/states.py
+++ fsm/states.py
@@ -1,9 +1,8 @@
 from enum import Enum
 
 class State(Enum):
-    AUTHENTICATING = "authenticating"
-    LOCKED = "locked"
-    UNLOCKING = "unlocking"
-    UNLOCKED = "unlocked"
-    LOCKING = "locking"
-    ERROR = "error" 
+    LOCKED = "locked"                # Door is locked
+    UNLOCKING = "unlocking"          # Door is unlocking
+    UNLOCKED = "unlocked"            # Door is unlocked
+    LOCKING = "locking"              # Door is locking
+    ERROR = "error"                  # Error occurred
