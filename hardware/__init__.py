d21e931af6d9460e71e699bcb8f129a9b89640d8
Jayson Ndungu <jaysongachiani@gmail.com>
2025-11-19 16:38:20 -0500

    removed bluetooth module

--- hardware/__init__.py
+++ hardware/__init__.py
@@ -1,10 +1,9 @@
 """
 Hardware module for SmartLock system.
-Contains motor control and Bluetooth communication functionality.
+Contains motor control functionality.
 """
 
 from .motor_controller import MotorController
-from .bluetooth_manager import BluetoothManager
 
-__all__ = ['MotorController', 'BluetoothManager']
+__all__ = ['MotorController']
 
