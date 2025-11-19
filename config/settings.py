d74591b32736159254ddb86be459fbbf224bd8bf
Jayson Ndungu <jaysongachiani@gmail.com>
2025-11-19 16:35:39 -0500

    added __init__ file to config to allow imports in other files

---
+++ config/settings.py
@@ -0,0 +1,12 @@
+Project_Name = "SmartLock"
+VERSION = "1.0.0"
+AUTHOR = "Jayson Ndungu"
+DESCRIPTION = "SmartLock is a system that allows you to lock and unlock your door using your phone. It utilizes a raspberry pi 5 to control the lock."
+
+
+# Pin Assignments
+
+MOTOR_PIN = 18
+IN1_PIN = 23
+IN2_PIN = 24
+SPEED = 0.5
