f476b3d0fc2a194a5f826150d3399000540d3e1e
Jayson Ndungu <jaysongachiani@gmail.com>
2025-11-25 14:06:45 -0500

    changed motor speed and how long the motor runs

--- config/settings.py
+++ config/settings.py
@@ -9,4 +9,4 @@ DESCRIPTION = "SmartLock is a system that allows you to lock and unlock your doo
 MOTOR_PIN = 18
 IN1_PIN = 23
 IN2_PIN = 24
-SPEED = 0.5
+SPEED = 0.5 #Speed of the motor can only be between 0 and 1.
